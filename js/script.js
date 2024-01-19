9a950126a1653169a0f05f243d6731b64275d155
bobg207 <rjg207@gmail.com>
2024-01-19 08:11:02 -0500

    4 card challenge

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 const plusBtns = document.querySelectorAll(".plus")
 const minusBtns = document.querySelectorAll(".minus")
 const answers = document.querySelectorAll(".faq-answer")
+const menuOpen = document.querySelector("#menu-open")
+const menuClose = document.querySelector("#menu-close")
+const linksMenu = document.querySelector(".nav")
+const currentYr = document.querySelector(".year")
+let menuIsOpen = false
 
+// #########################
+// update the copyright year
+// #########################
+const date = new Date()
+let year = date.getFullYear()
+currentYr.textContent = year
+
+
+// #########################
+// #########  FAQ  #########
+// #########################
 plusBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
-        let chosenBtn = btn.id
-        // in js if you multiply a str by 1 it gets converted to an int
-        chosenBtn = chosenBtn.slice(-1)*1
-        // console.log(answers[chosenBtn])
+        let chosenBtn = parseInt(btn.id)
         btn.style.display = 'none'
         minusBtns[chosenBtn].style.display = 'block'
         answers[chosenBtn].style.display = 'block'
@@ -16,12 +29,24 @@ plusBtns.forEach((btn) => {
 
 minusBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
-        let chosenBtn = btn.id
-        // in js if you multiply a str by 1 it gets converted to an int
-        chosenBtn = chosenBtn.slice(-1)*1
-        // console.log(answers[chosenBtn])
+        let chosenBtn = parseInt(btn.id)
         btn.style.display = 'none'
         plusBtns[chosenBtn].style.display = 'block'
         answers[chosenBtn].style.display = 'none'
     })
+})
+
+// ###############################
+// #########  Hamb Menu  #########
+// ###############################
+menuOpen.addEventListener('click', () => {
+    linksMenu.style.display = 'block'
+    menuClose.style.display = 'inline-block'
+    menuOpen.style.display = 'none'
+})
+
+menuClose.addEventListener('click', () => {
+    linksMenu.style.display = 'none'
+    menuClose.style.display = 'none'
+    menuOpen.style.display = 'inline-block'
 })
